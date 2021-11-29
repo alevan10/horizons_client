ed1c4becb9cc8816fd6e5080a9e2ae97ced0e7a2
alevan10 <levan.andrew.w@gmail.com>
2021-11-28 21:08:25 -0500

    adding tests and additional request objects

--- services/request_objects.py
+++ services/request_objects.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Union, Any
 
 from entities.enums import Planets, Moons, Observers
@@ -18,8 +19,27 @@ class BaseRequestObject(object):
 
 class BaseTimeRequest(BaseRequestObject):
 
+    time_format = "%Y-%b-%d %H:%M:%S.%f"
+
+    def __init__(self, value):
+        super().__init__(value)
+        if not isinstance(self.value, datetime):
+            raise ValueError("Value must be of type 'datetime'")
+
+
     def generate_request_param(self) -> str:
-        formatted_time =
+        formatted_time = self.value.strftime(self.time_format)
+        return f"{self.name.upper()}={formatted_time}"
+
+
+class StartTimeRequest(BaseTimeRequest):
+
+    name = "start_time"
+
+
+class StopTimeRequest(BaseTimeRequest):
+
+    name = "stop_time"
 
 
 class CommandRequestObject(BaseRequestObject):
